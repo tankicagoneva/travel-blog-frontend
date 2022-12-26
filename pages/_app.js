15a1148fcf7eecfe49155ed07e8f8fd1ba5a3b32
tankicagoneva <tankicaspasev@gmail.com>
2022-12-26 23:28:07 +0000

    travel blog frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import NavBar from '../components/NavBar'
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <div className='app-container'>
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
+
+export default MyApp
